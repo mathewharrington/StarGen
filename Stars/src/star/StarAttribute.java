25f7a17ca84a0f5db147bd185ad603c7beb8a825
Mathew Harrington <mathewharrington88@gmail.com>
2017-01-05 14:24:40 +1100

    moved star attributes to separate file

---
+++ Stars/src/star/StarAttribute.java
@@ -0,0 +1,20 @@
+package star;
+
+import java.awt.Color;
+
+public class StarAttribute {
+
+	public static final int SMALL_SIZE = 1;
+	public static final int MEDIUM_SIZE = 3;
+	public static final int LARGE_SIZE = 4;
+	
+	public static final Color SMALL_COLOR = new Color(167, 238, 253);
+	public static final Color MEDIUM_COLOR = new Color(208, 226, 236);
+	public static final Color LARGE_COLOR = new Color(217, 248, 255);
+	public static final Color SKY_COLOR = new Color(19, 22, 24);
+	
+	public StarAttribute() {
+		
+	}
+
+}
