8856a39f02c85d5f2ebfdce4630a8aec33ebfdcf
Mathew Harrington <mathewharrington88@gmail.com>
2017-01-05 14:50:51 +1100

    tidied up main method

--- Stars/src/Driver.java
+++ Stars/src/Driver.java
@@ -2,19 +2,10 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Driver {
-	private final static int WIDTH = 1800;
-	private final static int HEIGHT = 1200;
-	private final static double INITIAL_PROB = -0.9;
-	private final static double DUSTING_PROB = -0.7;
+	private final static int WINDOW_WIDTH = 1800;
+	private final static int WINDOW_HEIGHT = 1200;
 	
 	public static void main(String[] args) {
-		
-		// setup GUI environment
-		// take the menu bar off the jframe
-		System.setProperty("apple.laf.useScreenMenuBar", "true");
-
-		// set the name of the application menu item
-		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Star Generator");
 
 		// set the look and feel
 		try 
@@ -28,22 +19,21 @@ public class Driver {
 			e.printStackTrace();
 		}
 		
-		MainView view = new MainView(WIDTH, HEIGHT);
-		StarGenerator starGen = new StarGenerator(WIDTH, HEIGHT);
+		MainView view = new MainView(WINDOW_WIDTH, WINDOW_HEIGHT);
+		StarGenerator starGen = new StarGenerator(WINDOW_WIDTH, WINDOW_HEIGHT);
 		
 		// initial generation
-		starGen.genStars(INITIAL_PROB);	
+		starGen.generateInitial();
 		
-     	// second pass generation
-		starGen.genStars4(15);
-		starGen.genStars4(10);
-		//starGen.genStars4(10);
-		starGen.genStars4(25);
-		starGen.genStars4(6);
-		starGen.genStars4(60);
+     	// second pass cluster generation
+		starGen.generateCluster(15);
+		starGen.generateCluster(10);
+		starGen.generateCluster(25);
+		starGen.generateCluster(6);
+		starGen.generateCluster(60);
 		
 		// final dusting
-		starGen.genStars1(DUSTING_PROB);
+		starGen.generateDusting();
 		
 		Stars stars = new Stars(starGen.getStars());
 		
