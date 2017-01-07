f49d16bb06066e31fb1c0c51aa3f3fc064587558
Mathew Harrington <mathewharrington88@gmail.com>
2017-01-07 15:57:56 +1100

    adjusted dusting probability

--- Stars/src/star/StarAttribute.java
+++ Stars/src/star/StarAttribute.java
@@ -14,7 +14,7 @@ public class StarAttribute {
 	public static final Color SKY_COLOR = new Color(19, 22, 24);
 	
 	public static final double INITIAL_PROB = -0.9;
-	public static final double DUSTING_PROB = -0.7;
+	public static final double DUSTING_PROB = -0.75;
 	
 	public StarAttribute() {
 		
