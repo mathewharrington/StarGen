b46e88aeb00c3f8d45c4d909b420fff62c67f36a
Mathew Harrington <mathewharrington88@gmail.com>
2017-01-23 21:37:49 +1100

    adjusted star colour

--- Stars/src/star/StarAttribute.java
+++ Stars/src/star/StarAttribute.java
@@ -8,9 +8,12 @@ public class StarAttribute {
 	public static final int MEDIUM_SIZE = 3;
 	public static final int LARGE_SIZE = 4;
 	
-	public static final Color SMALL_COLOR = new Color(167, 238, 253);
-	public static final Color MEDIUM_COLOR = new Color(208, 226, 236);
-	public static final Color LARGE_COLOR = new Color(217, 248, 255);
+//	public static final Color SMALL_COLOR = new Color(167, 238, 253);
+//	public static final Color MEDIUM_COLOR = new Color(208, 226, 236);
+//	public static final Color LARGE_COLOR = new Color(217, 248, 255);
+	public static final Color SMALL_COLOR = new Color(221, 217, 247);
+	public static final Color MEDIUM_COLOR = new Color(224, 246, 255);
+	public static final Color LARGE_COLOR = new Color(152, 172, 237);
 	public static final Color SKY_COLOR = new Color(19, 22, 24);
 	
 	public static final double INITIAL_PROB = -0.9;
